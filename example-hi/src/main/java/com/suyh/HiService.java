100dbe4808877a03722cfcc2bd85b474f5f7254f
suyh <su_787910081@sina.com>
2023-01-20 14:46:42 +0800

    Update HiService.java

--- example-hi/src/main/java/com/suyh/HiService.java
+++ example-hi/src/main/java/com/suyh/HiService.java
@@ -4,6 +4,7 @@ public class HiService {
     private final SayHi sayHi = new SayHi();
 
     public void hi() {
+        System.out.println("HiService class loader: " + HiService.class.getClassLoader());
         sayHi.sayHi();
     }
 }
