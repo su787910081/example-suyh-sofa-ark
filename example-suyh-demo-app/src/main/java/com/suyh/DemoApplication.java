7dbaafc6127d90e706795f84b6b8c72e262937fc
suyh <su_787910081@sina.com>
2023-01-20 11:58:30 +0800

    Create DemoApplication.java

---
+++ example-suyh-demo-app/src/main/java/com/suyh/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.suyh;
+
+import com.alipay.sofa.ark.support.startup.SofaArkBootstrap;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+    public static void main(String[] args) {
+        SofaArkBootstrap.launch(args);
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
