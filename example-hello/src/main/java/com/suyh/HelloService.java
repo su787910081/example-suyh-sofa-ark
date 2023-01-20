5e113944fe179996e67e33fad3816389b96b4809
suyh <su_787910081@sina.com>
2023-01-20 14:46:59 +0800

    Update HelloService.java

--- example-hello/src/main/java/com/suyh/HelloService.java
+++ example-hello/src/main/java/com/suyh/HelloService.java
@@ -4,6 +4,7 @@ public class HelloService {
     private final SayHello sayHello = new SayHello();
 
     public void hello() {
+        System.out.println("HelloService class loader: " + HelloService.class.getClassLoader());
         sayHello.sayHello();
     }
 }
