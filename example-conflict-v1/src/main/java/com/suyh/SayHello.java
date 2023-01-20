437c6c89fc41aa1afd6d9fa3afaeae5fbe0f80f2
suyh <su_787910081@sina.com>
2023-01-20 14:34:01 +0800

    Update SayHello.java

--- example-conflict-v1/src/main/java/com/suyh/SayHello.java
+++ example-conflict-v1/src/main/java/com/suyh/SayHello.java
@@ -2,6 +2,7 @@ package com.suyh;
 
 public class SayHello {
     public void sayHello() {
-        System.out.println("hello, current example-conflict version is 1.0-SNAPSHOT");
+        System.out.println("hello, current example-conflict version is 1.0-SNAPSHOT, " +
+                "class loader: " + SayHello.class.getClassLoader());
     }
 }
